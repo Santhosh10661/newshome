f6d6e8ccd57e687bce6a1a829cd133557028d595
Santhosh10661 <138647625+Santhosh10661@users.noreply.github.com>
2023-09-06 16:07:47 +0530

    Update newshome.js

--- newshome.js
+++ newshome.js
@@ -1,6 +1,12 @@
-let menuBtn=document.querySelector(".menuBtn")
-let menuCloseBtn=document.querySelector(".menuCloseBtn")
-let menu = document.querySelector(".menu")
+let menuBtn = document.querySelector(".menuBtn");
+let menuCloseBtn = document.querySelector(".menuCloseBtn");
+let menu = document.querySelector(".menu");
 
-menuBtn.addEventListener('click',()=> menu.style.display="flex")
-menuCloseBtn.addEventListener('click',()=> menu.style.display="none")
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
+menuCloseBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  document.body.style.overflow = "scroll";
+});
